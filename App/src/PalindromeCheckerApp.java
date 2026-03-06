4e27caaeff9b055233814f3a8fad985acbdaabc8
Krrish-kh <krrish25k@gmail.com>
2026-03-06 15:13:32 +0530

    UC9: Recursive Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,31 +2,23 @@ package src;
 import java.util.*;
 import java.util.LinkedList;
 
-public class PalindromeCheckerApp {
+public class
+PalindromeCheckerApp {
     public static void main(String[] args) {
+        String input = "madam";
 
-        String input = "level";
+        boolean result = check(input, 0, input.length() - 1);
 
-        LinkedList<Character> list = new LinkedList<>();
-
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (list.size() > 1) {
-            char first = list.removeFirst();
-            char last = list.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        // Print result
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
+    }
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+        return check(s, start + 1, end - 1);
     }
 }
