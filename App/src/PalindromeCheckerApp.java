40363ec1c3f0ada46cbfc855fe3cfc3d33ee1310
Krrish <Krrish25k@gmail.com>
2026-03-06 15:17:18 +0530

    PalindromeCheckerApp

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class UseCase5PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
