db4fbf1efa2bfcdb02f58f95e371ad019dd6ff06
Krrish-kh <krrish25k@gmail.com>
2026-03-06 14:49:18 +0530

    UC7: Deque-Based Optimized Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,24 +1,27 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
+package src;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.*;
 
-public class UseCase6PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "civic";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            queue.add(c);      // Enqueue (FIFO)
-            stack.push(c);     // Push (LIFO)
-        }
-        boolean isPalindrome = true;
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+                String input = "refer";
+                Deque<Character> deque = new ArrayDeque<>();
+                // Add each character to the deque
+                for (char c : input.toCharArray()) {
+                    deque.addLast(c);
+                }
+                boolean isPalindrome = true;
+                while (deque.size() > 1) {
+                    char front = deque.removeFirst();
+                    char rear = deque.removeLast();
+
+                    if (front != rear) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                System.out.println("Input : " + input);
+                System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
